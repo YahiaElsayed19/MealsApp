f3dca7af91c439f47491441893d6075f99af679d
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-02-04 06:42:02 +0200

    initial

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,11 +1,35 @@
-import { useContext } from "react"
-import MealsList from "../components/MealsList"
-import { MEALS } from "../assets/data/dummy-data"
-import { FavoritesContext } from "../store/context/favorite-context"
+import { useContext } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import MealsList from "../components/MealsList";
+import { MEALS } from "../assets/data/dummy-data";
+import { FavoritesContext } from "../store/context/favorite-context";
 const FavoritesScreen = () => {
-    const favoriteMealsCtx = useContext(FavoritesContext)
-    const favoriteMeals = MEALS.filter((meal) => favoriteMealsCtx.ids.includes(meal.id))
-    return <MealsList items={favoriteMeals} />
-}
+    const favoriteMealsCtx = useContext(FavoritesContext);
+    const favoriteMeals = MEALS.filter((meal) =>
+        favoriteMealsCtx.ids.includes(meal.id)
+    );
+    if (favoriteMeals.length === 0) {
+        return (
+            <View style={styles.rootContainer}>
+                <Text style={styles.text}>
+                    You have no favorite meals yet.
+                </Text>
+            </View>
+        )
+    }
+    return <MealsList items={favoriteMeals} />;
+};
 
-export default FavoritesScreen
+export default FavoritesScreen;
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: "white",
+    },
+})
